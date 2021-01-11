62ef87516062e7cc6ce9d4c5dc53847b500b7592
unknown <robinkhan1984@gmail.com>
2021-01-11 17:27:13 -0500

    updates to sites

---
+++ Tables.sql
@@ -0,0 +1,35 @@
+
+CREATE TABLE Disasters (
+	name
+)
+
+
+
+
+
+femaDeclarationString
+disasterNumber
+state
+declarationType
+declarationDate
+fyDeclared
+incidentType
+declarationTitle
+ihProgramDeclared
+iaProgramDeclared
+paProgramDeclared
+hmProgramDeclared
+incidentBeginDate
+incidentEndDate
+disasterCloseoutDate
+fipsStateCode
+fipsCountyCode
+placeCode
+designatedArea
+declarationRequestNumber
+hash
+lastRefresh
+id
+
+
+
